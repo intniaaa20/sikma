64ad204d866c8db8851f6754167a42c3a4dcb138
Betran <beyranjr99@gmail.com>
2025-02-27 13:47:36 +0800

    betran

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+// GET route
+app.get('/', (req, res) => {
+    res.send('Hello GET from Express!');
+  });
+  
+  // POST route
+  app.post('/submit', (req, res) => {
+    res.send('Data has been submitted!');
+  });
